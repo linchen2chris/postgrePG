f6c2e300eaa6a09f3fca74d76379de5c91429a77
Lin Chen <clin@thoughtworks.com>
2019-10-02 17:00:02 +0800

    [Chen] add router for koa

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@
 import Koa from "koa";
 import { createConnection } from "typeorm";
 import { ormconfig } from "../ormconfig";
+import Router from "koa-router";
 
 createConnection(ormconfig);
 
 const app = new Koa();
+const router = new Router();
 
+router.get("/", (ctx, next) => {
+	// ctx.router available
+	ctx.body = "Hello World!";
+});
 const port = Number(process.env.PORT) || 4005;
-app.listen({ port }, () => {
+app.use(router.routes()).listen({ port }, () => {
 	console.log(`🏆 Server ready at http://localhost:${port}`);
 });
